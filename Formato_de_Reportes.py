835d7f5207d48eae84fbef368fe4564dacf4d6c9
Avmar422 <notnoot584@gmail.com>
2026-02-06 19:01:22 -0600

    Formato_de_Reportes.py

--- Formato_de_Reportes.py
+++ Formato_de_Reportes.py
@@ -4,5 +4,5 @@ Precio= float(input("Inserta el precio:"))
 
 Total= (Precio*Cantidad)
 
-print(f"Venta de {Producto}:{Cantidad} unidades a un precio de ${Precio}")
+print(f"Venta de {Producto}:{Cantidad} unidades a un precio de ${Precio} cada una")
 print(f"Total: ${Total}")
